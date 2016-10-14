0cadf32cd75e8da894336cef9c50b98450581624
kandorm <d-wan14@mails.tsinghua.edu.cn>
2016-10-14 14:36:54 +0800

    file finish and need record

--- filesystem/RecordManager.h
+++ filesystem/RecordManager.h
@@ -2,25 +2,38 @@
 #include "fileio/FileManager.h"
 #include "utils/pagedef.h"
 
+#define PAGE_NUM_BETWEEN_FREEPAGE 4096
+
 class RecordManager {
 private:
 	BufPageManager* bufPageManager;
-	bool initFileHead(const char* name, int recordSize) {
+	bool initFileHead(const char* name, int[] colSize, int colNum) {
+		
+		//记录的长度
+		int recordSize = 0;
+		for (int i = 0; i < colNum; i++)
+			recordSize += colSize[i];
+
 		int fileID;
 		int index;
 		bufPageManager->fileManager->openFile(name, fileID);
 		BufType b = bufPageManager->allocPage(fileID, 0, index, false);	//SIZE=PAGE_INT_NUM=2048
+		
 		b[0] = recordSize;												//文件中记录长度
 		b[1] = PAGE_SIZE / recordSize;									//文件每一页可以存储最多记录数
 		b[2] = 0;														//文件中记录个数
+		b[3] = colNum;													//表的列数
+		for (int i = 4; i < colNum + 4; i++)							//表每一列的size
+			b[i] = colSize[i - 4];
+
 		bufPageManager->markDirty(index);
 		bufPageManager->writeBack(index);
 	}
 public:
 	//File Operation
-	bool createFile(const char* name, int recordSize) {
+	bool createFile(const char* name, int[] colSize, int colNum) {
 		if (bufPageManager->fileManager->createFile(name)) {
-			if (initFileHead(name, recordSize))
+			if (initFileHead(name, colSize, colNum))
 				return true;
 			remove(name);
 			return false;
@@ -40,22 +53,34 @@ public:
 	}
 
 	//Record Operation
-	bool insertRecord(int fileID, unsigned char[] record, int& rID) {
+	bool insertRecord(int fileID, unsigned char[] record, int& recordID) {
 		int index;
 		int recordSize;
-		int recordNum;
+		int recordNumPerFile;
 		BufType b = bufPageManager->getPage(fileID, 0, index);
-		recordSize = b[0];	//该文件中记录的长度（记录定长，最长UNSIGNED_MAX_INT）
-		recordNum = b[1];   //该文件中记录总个数
+		recordSize = b[0];			//该文件中记录的长度（记录定长，最长UNSIGNED_MAX_INT）
+		recordNumPerFile = b[1];	//每一页中最大记录数
+
+		b[2] += 1;
+		bufPageManager->markDirty(index);
+
+
+
 
 	}
-	bool deleteRecord() {
+	bool deleteRecord(int fileID, int pageID, int recordID) {
+		int fileHeadIndex;
+		int recordSize;
+		BufType fileHeadBuf = bufPageManager->getPage(fileID, 0, fileHeadIndex);
+		recordSize = fileHeadBuf[0];
+		
+		BufType buf = bufPageManager->getPage(fileID, pageID, index);
 
 	}
 	bool updateRecord() {
 
 	}
-	bool getRecord() {
+	int[][3] getRecord(int fileID) {
 
 	}
 	RecordManager(BufPageManager* bpm) {
